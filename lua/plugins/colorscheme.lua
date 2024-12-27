d8894bde015f7f08e5071dfb591fad134eb12614
Bryan Liang <bryanliang1995@gmail.com>
2024-12-27 12:48:48 -0800

    Update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,10 +4,30 @@ return {
   { "catppuccin/nvim", name = "catppuccin" },
   { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
   {
+    "0xstepit/flow.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      theme = {
+        style = "dark", -- "dark" or "light"
+        contrast = "default", -- "default" or "high"
+        transparent = false,
+      },
+      colors = {
+        mode = "default",
+        fluo = "yellow", -- "pink", "cyan", "yellow", "orange", "green"
+      },
+      ui = {
+        borders = "inverse", -- "theme", "inverse", "fluo", "none"
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "gruvbox",
-      colorscheme = "catppuccin-frappe",
+      -- colorscheme = "catppuccin-frappe",
+      colorscheme = "flow",
       -- backgrouud = "dark",
     },
   },
